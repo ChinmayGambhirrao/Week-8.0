4d0651b6b39ed10fae90940c6043624321b3832a
CHINMAY <ccgambhirrao@gmail.com>
2025-04-20 19:36:58 +0530

    Start of ytclone

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,25 @@
+
+export function VideoCard(props: any) {
+    return (
+        <div>
+            <img className="rounded-xl" src={props.image}></img>
+            <div className="grid grid-cols-12 pt-2">
+                <div className="col-span-1">
+                    <img className="rounded-full w-12 h-12" src={props.thumbImage}></img>
+                </div>
+                <div className="col-span-11">
+                    <div>
+                    {props.title}
+                    <div className="col-span-11 text-gray-400 text-base">
+                    {props.author}
+                </div>
+                <div className="col-span-11 text-gray-400 text-base">
+                    {props.views} . {props.time}
+                </div>
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+    )
+}
